17c9b1769c6ce601cb3c6c673beda958a5d5d0ec
Dojo <mhlengijojo7@gmail.com>
2023-01-18 01:08:22 +0200

    Ended at Vue.js Beginners #3 10:45

--- vue-firebase/app.js
+++ vue-firebase/app.js
@@ -6,7 +6,9 @@ const app = Vue.createApp({
             showbooks: true,
             title: 'Things Change',
             author: 'Okonkwo',
-            age: 126
+            age: 126,
+            x: 0,
+            y: 0
         }
     },
 
@@ -18,6 +20,18 @@ const app = Vue.createApp({
 
         toggleBooks(){
             this.showbooks = !this.showbooks
+        },
+
+        handleEvent(e, data){
+            console.log(e, e.type)
+            if(data != null){
+                console.log(data)
+            }
+        },
+
+        handleMousemove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
