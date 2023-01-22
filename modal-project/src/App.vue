e84b13f274380542c533e2d0fd2fb377da85ea24
Dojo <mhlengijojo7@gmail.com>
2023-01-22 16:22:52 +0200

    Blank page problem solved by not using Vite. Now on NetNinja #5 learning Props

---
+++ modal-project/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click Me</button>
+  <Modal :header="header" :text="text" theme ="sale"/>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App by DzowDzow :)',
+      header: 'Sign up for the giveaway!',
+      text: 'Grab your ninja swag for half price!',
+    }
+  },
+
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  }
+}
+</script>
+
+<style>
+
+@import './assets/global.css';
+
+#app {
+  font-family: Avenir, Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top:60px;
+}
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="backdrop">
+        <div class="modal" :class="{sale:theme === 'sale'}">
+            <h1>{{ header }}</h1>
+            <p>{{ text }}</p>
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+export default{
+    props: ['header', 'text']
+}
+
+</script>
+
+
+<style>
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+}
+
+h1 {
+    border-bottom: 1px solid rgb(221, 221, 221);
+    display: inline-block;
+    padding-bottom: 10px;
+    color: rgb(11, 124, 134);
+}
+
+.modal p{
+    font-style: normal;
+}
+</style>
