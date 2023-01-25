3d2ebc959a9a027e248c3e0ba9afb5db0d3c3622
Dojo <mhlengijojo7@gmail.com>
2023-01-25 06:45:28 +0200

    Continuing previous modal-project to avaoid deleting any elements like he has. Still on Video #5. Beginning Slots

---
+++ modal-project-2/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click Me</button>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme ="sale" @close="toggleModal"/>
+  </div>
+  <button @click.alt="toggleModal">Toggle Modal (alt)</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App by DzowDzow :)',
+      header: 'Sign up for the giveaway!',
+      text: 'Grab your ninja swag for half price!',
+      showModal: false
+    }
+  },
+
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    },
+
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
+  }
+}
+</script>
+
+<style>
+
+@import './assets/global.css';
+
+#app {
+  font-family: Avenir, Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top:60px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+}
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ modal-project-2/src/components/Modal.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="backdrop" @click.self="closeModal"> <!-- la . self makes it work on backdrop only and not on anything else inside th element, cause if akekho it will still trigger after clicking on Modal-->
+        <div class="modal" :class="{sale:theme === 'sale'}"> <!--is equal to a boolean, if its true izovela, if its not true ayizuvela-->
+            <h1>{{ header }}</h1>
+            <p>{{ text }}</p>
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+export default{
+    //"registering it"
+    props: ['header', 'text', 'theme'],
+
+    methods: {
+        closeModal(){
+            //creating custom event to be used in App.Vue js
+            //like binding in a way, makes the component in App.vue listen to the event
+            // makes it to be used like u@click, but it will be called @close in app, then calls function
+            this.$emit('close')
+        }
+    }
+}
+
+</script>
+
+
+<style>
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+
+.backdrop {
+    top: 0;
+    left:0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+}
+
+h1 {
+    border-bottom: 1px solid rgb(221, 221, 221);
+    display: inline-block;
+    padding-bottom: 10px;
+    color: rgb(11, 124, 134);
+}
+
+.modal p{
+    font-style: normal;
+}
+
+.modal.sale{
+    background: crimson;
+    color: white;
+}
+
+.modal.sale h1{
+    color: white;
+}
+
+</style>
