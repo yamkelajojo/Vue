ecdfd8480566470c00e57ba2dd209c1626159fe8
Dojo <mhlengijojo7@gmail.com>
2023-01-26 05:04:29 +0200

    Just setup environment for reaction timer project

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Dojo Reaction Timer</h1>
+  <RT></RT>
+</template>
+
+<script>
+import Results from './components/Results.vue'
+import Block from './components/Block.vue'
+
+
+export default {
+  name: 'App',
+  components: {Block, Results}
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
