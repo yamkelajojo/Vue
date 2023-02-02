b527e43ed60f02e6c89c46b7d187869545bef1b8
Dojo <mhlengijojo7@gmail.com>
2023-02-02 05:38:34 +0200

    Completed Router video, video #8. Moving on to video #9

--- ninja-jobs/src/App.vue
+++ ninja-jobs/src/App.vue
@@ -4,8 +4,28 @@
     <router-link :to="{name: 'About'}">About</router-link> |
     <router-link :to="{name: 'Jobs'}">Jobs</router-link>
   </nav>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go Back</button>
+  <button @click="forward">Go Forward</button>
   <router-view/> <!--this is where our components will show-->
 </template>
+
+<script>
+  export default{
+    methods: {
+      redirect(){
+        this.$router.push({name: 'home'})
+      },
+      back(){
+        this.$router.go(-1)
+      },
+      forward(){
+        this.$router.go(+1)
+      }
+    }
+  }
+</script>
 
 <style>
 #app {
@@ -31,5 +51,12 @@ nav a {
 nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+
+button{
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px
 }
 </style>
--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Jobs from '../views/jobs/Jobs.vue'
 import JobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -24,9 +25,20 @@ const routes = [
     path: '/jobs/:id',
     name: 'JobDetails',
     component: JobDetails,
-    props: true
-  }
+    props: true //make it usable as a prop.
+  },
+  //redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
 
+  },
+  //catch all 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
+  }
 ]
 
 const router = createRouter({
