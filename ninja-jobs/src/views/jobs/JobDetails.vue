8a5d9572049e9ba819bc6fbb284e3cecae54de44
Dojo <mhlengijojo7@gmail.com>
2023-02-03 01:57:09 +0200

    Just Finished video 9 which was about JSON Server and fetching data

--- ninja-jobs/src/views/jobs/JobDetails.vue
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,16 +1,34 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The job id is: {{ id }}</p> 
+  <div v-if="job">
+    <h1>{{job.title}}</h1>
+    <p>The job id is: {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Page is still loading...</p>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['id']
+    props: ['id'],
     // data(){
     //     return{
     //         id: this.$route.params.id
     //     }
     // }
+
+    data(){
+      return{
+        job: null
+      }
+    },
+    mounted() {
+      fetch('http://localhost:3000/jobs/' + this.id)
+        .then(res => res.json())
+        .then(data => this.job = data)
+        .catch(err => console.log(err.messsage)) //to catch errors and display them in console
+  }
 }
 </script>
 
