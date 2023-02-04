b6eeaf9bee1d43a98fa122f69f2d9221178605fd
Dojo <mhlengijojo7@gmail.com>
2023-02-04 05:14:34 +0200

    Just learnt use of ref. Will be continue in Dojo-blog2

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -3,12 +3,14 @@
     <p>Home</p>
     <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import {ref} from 'vue'
+import {ref, reactive} from 'vue'
 
 export default {
   name: 'Home ',
@@ -16,13 +18,12 @@ export default {
   setup(){
 
     const p = ref(null) // assigned it a vule of null
-    let name = 'mario'
-    let age = 30
+    const name = ref('mario')
+    const age = ref(30) // a contants refs value can change. use refs to make em accessible in template.refs are reactive values
 
     const handleClick = () => {
-      console.log(p, p.value)
-      p.value.classList.add('test')
-      p.value.textContent = 'hello ninjas'
+      name.value = 'luigi'
+      age.value = 35
     }
 
     return{ name, age, handleClick, p} //must return and must return them as an object if you wanna use them in template
