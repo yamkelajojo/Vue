1b6dd83d2ce40cc8d0ff72f78f7643ac04ef7de8
Dojo <mhlengijojo7@gmail.com>
2023-02-04 04:58:25 +0200

    Just set up Dojo-blog for Video #10, now getting into use of refs in composition API

---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <p>Home</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import {ref} from 'vue'
+
+export default {
+  name: 'Home ',
+
+  setup(){
+
+    const p = ref(null) // assigned it a vule of null
+    let name = 'mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'hello ninjas'
+    }
+
+    return{ name, age, handleClick, p} //must return and must return them as an object if you wanna use them in template
+
+  }
+}
+</script>
