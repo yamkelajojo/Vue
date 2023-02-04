d414b48c6c5e9e2697c0d3180294584ae7cbc1ed
Dojo <mhlengijojo7@gmail.com>
2023-02-04 06:53:15 +0200

    Just learnt about computed properties. still on vid #10. about to start watch and watch effect

---
+++ dojo-blog-2/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <input type="text" v-model="search">
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { ref, computed } from 'vue'
+
+export default {
+  name: 'Home ',
+
+  setup() {
+    const search = ref('')
+    const names = ref(['dojo', 'quakes', 'scooby', 'may', 'josta', 'nendro'])
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+
+    return { names, search, matchingNames }
+  }
+}
+</script>
+
