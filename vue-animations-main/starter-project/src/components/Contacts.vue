53c374fe32a73ad1ab571db59c8087d4cf1ed8af
Dojo <mhlengijojo7@gmail.com>
2023-02-11 17:54:37 +0200

    did the cards for contacts. NOW we're ready to start with the lesson

---
+++ vue-animations-main/starter-project/src/components/Contacts.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+
+// import { ref } from 'vue';
+export default {
+    name: 'Contacts',
+}
+</script>
+
+<style>
+.contactDiv{
+  display: grid;
+  grid-template-columns: repeat(1fr 1fr);
+  /* place-items: center; */
+  text-align: center;
+  background-color: rgb(122, 122, 185);
+  min-width: 400px;
+}
+</style>
--- vue-animations-main/starter-project/src/views/Contact.vue
+++ vue-animations-main/starter-project/src/views/Contact.vue
@@ -1,13 +1,52 @@
 <template>
-  Contact view
+  <h1>Contact</h1>
+  <div class="contactDiv">
+    <div class="contact" v-for="contact in contacts" :key="contact.id"><h4>{{ contact.title }}</h4></div>
+  </div>
 </template>
 
-<script>
-export default {
 
+<script>
+
+// import Contacts from '../components/Contacts.vue'
+import { ref } from 'vue';
+
+
+export default {
+  // components: {Contacts}
+  setup() {
+    const contacts = ref([
+      { title: "Email", id: 1 },
+      { title: "Phone", id: 2 },
+      { title: "Post", id: 3 },
+      { title: "Contact Signal", id: 4 }
+    ])
+
+    return { contacts }
+  }
 }
 </script>
 
 <style>
+.contactDiv {
+  width: 400px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  
+  place-items: center;
+  text-align: center;
+  /* background-color: rgb(193, 213, 203); */
+}
 
+.contact{
+  background-color: white;
+  border-radius: 6px;
+  min-width: 170px;
+  min-height: 120px;
+  margin: 12px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: rgba(70, 70, 70, 0.24) 0px 2px 7px;
+}
 </style>
