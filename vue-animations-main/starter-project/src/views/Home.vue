0fa6fd7c612af5547315391e9067379ba7e586b3
Dojo <mhlengijojo7@gmail.com>
2023-02-13 00:05:08 +0200

    Just completed Video #3, now we're about to add animation to the pink toast widget that appears on top

--- vue-animations-main/starter-project/src/views/Home.vue
+++ vue-animations-main/starter-project/src/views/Home.vue
@@ -3,7 +3,11 @@
     <Toast v-if="showToast"/>
     <Todos @badValue = "triggerToast"/>
   </div>
+  <transition name="fade">
+    <div v-if="showP">hello, ninjas</div>
+  </transition>
 
+  <button @click="showP=!showP">toggle</button>
 </template>
 
 <script>
@@ -19,16 +23,40 @@ export default {
 
   setup(){
     const showToast = ref(false)
+    const showP = ref(false)
 
     const triggerToast = () => {
       showToast.value = true;
       setTimeout(() => showToast.value = false, 3000)
     }
 
-    return {showToast, triggerToast}
+    return {showToast, triggerToast, showP}
   }
 }
 </script>
 
 <style>
+.fade-enter-from{
+  opacity: 0;
+}
+
+.fade-enter-to{
+  opacity: 1
+}
+
+.fade-enter-active{
+  transition: all 2s ease;
+}
+
+.fade-leave-from{
+  opacity: 1;
+}
+
+.fade-leave-to{
+  opacity: 0;
+}
+
+.fade-leave-active{
+  transition: all 2s ease;
+}
 </style>
