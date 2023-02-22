5edf29d7bb234fce422e22e2c090e8640b9327d5
yamkelajojo <yajojo022@student.wethinkcode.co.za>
2023-02-22 17:22:14 +0200

    Done video #10

--- vue-animations-main/starter-project/src/views/Contact.vue
+++ vue-animations-main/starter-project/src/views/Contact.vue
@@ -1,35 +1,58 @@
 <template>
   <div class="contact">
     <h1>Contact</h1>
-    <transition-group class="contact" tag="">
-      <li class="contact" v-for="icon in icons" :key="icon.name" >
+    <transition-group
+      appear
+      class="contact"
+      tag="ul"
+      @before-enter="beforeEnter"
+      @enter="enter"
+    >
+      <li
+        class="contact"
+        v-for="(icon, index) in icons"
+        :key="icon.name"
+        :data-index="index"
+      >
         <span class="material-icons">{{ icon.name }}</span>
-        <div>{{icon.text}}</div>
+        <div>{{ icon.text }}</div>
       </li>
     </transition-group>
   </div>
 </template>
 
-
 <script>
-
 // import Contacts from '../components/Contacts.vue'
-import { ref } from 'vue';
-
+import { ref } from "vue";
+import gsap from "gsap";
 
 export default {
   // components: {Contacts}
   setup() {
     const icons = ref([
-      {name:'alternate_email', text:"by email" },
-      { name: 'local_phone', text: "by phone" },
+      { name: "alternate_email", text: "by email" },
+      { name: "local_phone", text: "by phone" },
       { name: "local_post_office", text: "by post" },
-      { name: "local_fire-department", text: "by smoke signal" }
-    ])
+      { name: "local_fire-department", text: "by smoke signal" },
+    ]);
 
-    return { icons }
-  }
-}
+    const beforeEnter = (el) => {
+      el.style.opacity = 0;
+      el.style.transform = "translateY(100px)";
+    };
+
+    const enter = (el, done) => {
+      gsap.to(el, {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+        onComplete: done,
+        delay: el.dataset.index * 0.2,
+      });
+    };
+    return { icons, beforeEnter, enter };
+  },
+};
 </script>
 
 <style>
@@ -43,12 +66,12 @@ export default {
   /* background-color: rgb(193, 213, 203); */
 }
 
-.contact li{
-  list-style-type: none ;
+.contact li {
+  list-style-type: none;
   background: white;
   border-radius: 10px;
   padding: 30px;
-  box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+  box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
   cursor: pointer;
   line-height: 1.5em;
 }
