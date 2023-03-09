8cf4b83e7263c42be05123d67fb76fcaac66487b
yamkelajojo <yajojo022@student.wethinkcode.co.za>
2023-03-09 17:10:31 +0200

    Added nuxt project

---
+++ nuxt-dojo/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <nuxt-link to="/" class="font-bold">Nuxt Dojo</nuxt-link>
+        <ul class="flex gap-4">
+          <li><nuxt-link to="/">Home</nuxt-link></li>
+          <li><nuxt-link to="/about">About</nuxt-link></li>
+          <li><nuxt-link to="/products">Products</nuxt-link></li>
+        </ul>
+        <a href="/about">normal link --- about page</a>
+      </nav>
+    </header>
+
+    <!--output the page content-->
+    <div class="container mx-auto p-4">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
---
+++ nuxt-dojo/pages/products/[id].vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, modi eum
+      enim expedita quos cum delectus provident quaerat quia quidem nam dolores,
+      cumque facilis ad magni non recusandae numquam officiis.12
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+
+definePageMeta({
+  layout: "products",
+});
+</script>
+
+<style scoped></style>
