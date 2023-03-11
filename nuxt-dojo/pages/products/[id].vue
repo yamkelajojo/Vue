e05a9cd96a6af4165051277f5a66ef6995e31c1d
yamkelajojo <yajojo022@student.wethinkcode.co.za>
2023-03-11 16:32:38 +0200

    Done with NetNinja Nuxt vid#10, going to 11 now

--- nuxt-dojo/pages/products/[id].vue
+++ nuxt-dojo/pages/products/[id].vue
@@ -11,6 +11,15 @@ const uri = "https://fakestoreapi.com/products/" + id;
 //fetch the product
 const { data: product } = await useFetch(uri);
 
+//if product does not have a value  fatal:true makes it show when link to is clicked on, not just when its typed in
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Product not found",
+    fatal: true,
+  });
+}
+
 definePageMeta({
   layout: "products",
 });
